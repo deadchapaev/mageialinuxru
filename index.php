36a681d40ed47814121e13ab79aa95d5d99e6704
deadchapaev <chapaevcs@gmail.com>
2014-10-05 01:08:53 +0300

    Первая версия вёрстки

---
+++ index.php
@@ -0,0 +1,160 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/960px.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+
+</head>
+
+
+<body>
+
+<div id="container" class="container_12">
+    <div id="header">
+        <!--шапка-->
+        <div class="grid_4">
+            <img src="images/logo_small.png">
+        </div>
+        <div id="flowertop" class="grid_8">
+            <img src="images/flower-top.png">
+        </div>
+        <div class="clear"></div>
+
+        <!--меню-->
+        <div id="navbar" class="grid_12">
+            <div id="menu" class="grid_9">
+                <ul>
+                    <li>Главная</li>
+                    <li>Загрузки</li>
+                    <li>Вики</li>
+                    <li>Репозиторий</li>
+                    <li>Форум</li>
+                </ul>
+            </div>
+            <div class="grid_3">
+                <div id="searchbar"></div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+
+    <div class="content">
+        <!--Описание дистра-->
+        <div class="grid_6 about">
+            <div class="info">
+                Mageia - это стабильная и безопасная, свободная операционная система для персональных компьютеров и
+                серверов, основанная на GNU/Linux.
+            </div>
+            <div class="detail">
+                Тысячи людей и десятки организаций по всему миру выбирают Mageia для повседневного использования дома и
+                в офисе.
+            </div>
+            <div class="button">Загрузите прямо сейчас</div>
+        </div>
+
+        <!--Монитор-->
+        <div class="grid_6 monitor">
+            <div id="flowerbottom" class="grid_8">
+                <img src="images/flower-bottom.png">
+            </div>
+
+            <img src="images/monitor.png">
+
+
+        </div>
+        <div class="clear"></div>
+
+        <!--Линия-->
+        <div class="grid_12 grad_line"></div>
+        <div class="clear"></div>
+
+        <!--Новости-->
+        <div class="grid_12">
+            <H3>Последние новости</H3>
+        </div>
+        <div class="grid_12">
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+            <div class="news">
+                <H4>Вышла Mageia 3 beta!</H4>
+                <div class="news_body">
+                    Вышла Mageia 3 Beta 1.
+                    Это первый релиз, содержащий в себе некоторые изменения в инсталляторе. Например, доступна опция
+                    использования GRUB2 и т.д.
+                </div>
+                <div class="button_read">Читать!</div>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="clear"></div>
+    </div>
+
+
+    <!--Линия-->
+    <div class="grid_12 grad_line"></div>
+    <div class="clear"></div>
+
+
+    <!--Футер-->
+    <div class="footer">
+        <div id="footer" class="grid_12">
+
+                <img src="images/logo.png">
+
+
+            <div class="footer_menu">
+                Карта сайта
+                <ul>
+                    <li>Главная</li>
+                    <li>Загрузки</li>
+                    <li>Вики</li>
+                    <li>Репозиторий</li>
+                    <li>Форум</li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+
+</body>
