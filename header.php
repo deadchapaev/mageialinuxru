e8165ee2084f159f6f8676ef15e701bd7bb5736d
deadchapaev <chapaevcs@rambler.ru>
2014-10-09 11:27:38 +0300

    Удалил строку поиска

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 </head>
 
 <body>
-
 <div id="container" class="container_12">
     <div id="upline" class="grid_12"></div>
     <div class="clear"></div>
@@ -20,7 +19,7 @@
     <div class="clear"></div>
     <!--меню-->
     <div id="navbar" class="grid_12">
-        <div id="menu" class="grid_8 alpha">
+        <div id="menu" class="grid_12 alpha">
             <ul>
                 <li>Главная</li>
                 <li>Загрузки</li>
@@ -28,12 +27,6 @@
                 <li>Репозиторий</li>
                 <li>Форум</li>
             </ul>
-        </div>
-        <div id="searchbar" class="grid_4">
-            <form name="searchin" method="post" action="index.php">
-                <input id="searchinput" type="text" size="25">
-                <input id="searchsubmit" type="submit" value="">
-            </form>
-        </div>
+        </div>        
     </div>
     <div class="clear"></div>
