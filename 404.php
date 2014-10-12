a5879ea61cc40c23f5838322f60274d1d78432d7
jjackass <jonnyjackass@qip.ru>
2014-10-12 16:06:28 +0300

    added 404

--- 404.php
+++ 404.php
@@ -1,6 +1,9 @@
 <?php
 get_header(); ?>
-	<div id="content" class="grid_12">
-		Извините, страница не найдена!
-	</div>
+    <div id="content" class="grid_12">
+        <div id="mes"><p align="center">Извините, страница не найдена!</p></div>
+        <div id="pinguin" class="grid_6 push_4"></div>
+    </div>
+    <div class="clear"></div>
+        <hr>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/news.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/download.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/404.css"/>
     <title>mageialinux.ru</title>
 </head>
 
