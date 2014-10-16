176d4ddc5dd48cf5af2151d16ecc743788e9dd96
deadchapaev <chapaevcs@gmail.com>
2014-10-16 23:24:29 +0300

    Переверстал меню и кнопки

--- footer.php
+++ footer.php
@@ -3,21 +3,11 @@
     <div class="footer_menu">
         <p>Карта сайта
         <ul>
-            <a href="/">
-                <li>Главная</li>
-            </a>
-            <a href="/download">
-                <li>Загрузки</li>
-            </a>
-            <a href="http://forum.mageialinux.ru/">
-                <li>Форум</li>
-            </a>
-            <a href="http://wiki.mageialinux.ru/index.php?title=%D0%9F%D0%BE%D0%B4%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D1%8F_Mageia_Russian_Community">
-                <li>Репозиторий</li>
-            </a>
-            <a href="http://wiki.mageialinux.ru/index.php?title=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0">
-                <li>Вики</li>
-            </a>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/download">Загрузки</a></li>
+            <li><a href="http://forum.mageialinux.ru/">Форум</a></li>
+            <li><a href="http://wiki.mageialinux.ru/index.php?title=%D0%9F%D0%BE%D0%B4%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D1%8F_Mageia_Russian_Community">Репозиторий</a></li>            
+            <li><a href="http://wiki.mageialinux.ru/index.php?title=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0">Вики</a></li>            
         </ul>
     </div>
     <div class="grid_5 prefix_3 copyright">copyright 2014 - Mageia Russian Community</div>
