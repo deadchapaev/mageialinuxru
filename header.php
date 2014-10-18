ecb6f932c1cd825cc514d6a4b08a6d774f503445
jjackass <jonnyjackass@qip.ru>
2014-10-18 20:37:41 +0300

    header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/960px.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/news.css"/>
@@ -13,23 +13,32 @@
 
 <body>
 <div id="container" class="container_12">
-    <div id="upline" class="grid_12"></div>
+    <!--    <div id="upline" class="grid_12"></div>-->
     <div class="clear"></div>
 
     <!--шапка-->
-    <div id="logo" class="grid_4"><a href="/"></a></div>
-    <div id="flowertop" class="grid_8 omega"></div>
-    <div class="clear"></div>
+    <!--    <div id="logo" class="grid_4"><a href="/"></a></div>-->
+    <!--    <div id="flowertop" class="grid_8 omega"></div>-->
+    <!--    <div class="clear"></div>-->
     <!--меню-->
-    <div id="navbar" class="grid_12 alpha">
-        <div id="menu" class="grid_12 alpha">
-            <ul>                
+    <div id="navbar" class="grid_12">
+        <div id="logo"><a href="/"></a></div>
+        <div id="menu">
+            <ul>
                 <li><a href="/">Главная</a></li>
                 <li><a href="/download">Загрузки</a></li>
                 <li><a href="http://forum.mageialinux.ru/">Форум</a></li>
-                <li><a href="http://wiki.mageialinux.ru/index.php?title=%D0%9F%D0%BE%D0%B4%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D1%8F_Mageia_Russian_Community">Репозиторий</a></li>                
-                <li><a href="http://wiki.mageialinux.ru/index.php?title=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0">Вики</a></li>
+                <li>
+                    <a href="http://wiki.mageialinux.ru/index.php?title=%D0%9F%D0%BE%D0%B4%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D1%8F_Mageia_Russian_Community">Репозиторий</a>
+                </li>
+                <li>
+                    <a href="http://wiki.mageialinux.ru/index.php?title=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0">Вики</a>
+                </li>
             </ul>
         </div>
     </div>
     <div class="clear"></div>
+
+    <HR id="headerhr">
+
+
