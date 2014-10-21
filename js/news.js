398666b6103eb42634dbeb390a3b7a1db1b3937f
deadchapaev <chapaevcs@gmail.com>
2014-10-21 15:24:53 +0300

    подправил время

--- js/news.js
+++ js/news.js
@@ -9,13 +9,17 @@ $(document).on('click', '#leftarrow', function () {
 function left_carusel() {
     $a = $("#newsblock .news").size();
 	$curblock = $("#newsblock .news").eq($a - 1);
-	$curblock.hide();
-	$($curblock).prependTo("#newsblock").animate({ width: 'show'}, 200);
+	$curblock.hide()
+	$($curblock).prependTo("#newsblock").animate({ width: 'show'}, 100);
+		
+	
+	
+	
 }
 
 function right_carusel() {
 	$curblock = $("#newsblock .news").eq(0);
-    $($curblock).animate({ width: 'hide' }, 200, function() {
+    $($curblock).animate({ width: 'hide' }, 100, function() {
 		$($curblock).appendTo("#newsblock").show(); 
 	}); 
 }
