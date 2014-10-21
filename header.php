8b6ac0f70f0aba91548cf005ab653e2b752e3a9a
jjackass <jonnyjackass@qip.ru>
2014-10-21 14:59:26 +0300

    little fixes

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
                     <a href="/mrcrepo">Репозиторий</a>
                 </li>
 				<li><a href="http://forum.mageialinux.ru/">Форум</a></li>
-                <li>
+                <li id="liborder">
                     <a href="http://wiki.mageialinux.ru/index.php?title=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0">Вики</a>
                 </li>
             </ul>
