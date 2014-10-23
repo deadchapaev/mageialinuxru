a24eb323f5d2ee8e224546385323194b7369559f
jjackass <jonnyjackass@qip.ru>
2014-10-23 11:30:45 +0300

    121212

--- footer.php
+++ footer.php
@@ -1,47 +1,54 @@
 <!--Футер-->
 <div id="footer" class="grid_12">
+    <div class="footer_top">
+
+    </div>
+
+    <div class="footer_bottom">
+
+    </div>
 <!--	<hr>-->
 
-    <div id="social" class="grid_4 alpha">
-        <p>
-            Вы можете связаться с нами:
-        </p>
-
-        <div id="socicons">
-            <ul>
-                <li id="ok"><a href="/"></a></li>
-                <li id="g"><a href="/"></a></li>
-                <li id="tw"><a href="https://twitter.com/Mageia_RUS"></a></li>
-                <li id="mail"><a href="/"></a></li>
-                <li id="vk"><a href="http://vk.com/mrc.mageia"></a></li>
-            </ul>
-        </div>
-        <div id="otherinfo">
-            <p>
-                <b>Поддержка</b><br><br>
-                <b>icq:</b> 12345678910 <br>
-                <b>skype</b>: mageialinuxru
-            </p>
-        </div>
-    </div>
+<!--    <div id="social" class="grid_4 alpha">-->
+<!--        <p>-->
+<!--            Вы можете связаться с нами:-->
+<!--        </p>-->
+<!---->
+<!--        <div id="socicons">-->
+<!--            <ul>-->
+<!--                <li id="ok"><a href="/"></a></li>-->
+<!--                <li id="g"><a href="/"></a></li>-->
+<!--                <li id="tw"><a href="https://twitter.com/Mageia_RUS"></a></li>-->
+<!--                <li id="mail"><a href="/"></a></li>-->
+<!--                <li id="vk"><a href="http://vk.com/mrc.mageia"></a></li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--        <div id="otherinfo">-->
+<!--            <p>-->
+<!--                <b>Поддержка</b><br><br>-->
+<!--                <b>icq:</b> 12345678910 <br>-->
+<!--                <b>skype</b>: mageialinuxru-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </div>-->
 	
 	
-	<div class="footer_menu grid_2 push_6 alpha">
-        <p>Карта сайта
-        <ul>
-            <li><a href="/">Главная</a></li>
-            <li><a href="/download">Загрузки</a></li>
-            <li><a href="/mrcrepo">Репозиторий</a></li>            
-			<li><a href="http://forum.mageialinux.ru/">Форум</a></li>
-			<li><a href="http://wiki.mageialinux.ru/">Вики</a></li>            
-			<li>&nbsp;</li>
-			<li><a href="/arch">Архив</a></li>
-			<li><a href="/mrc-feed">mrc-feed</a></li>
-        </ul>
-    </div>
-	<div class="clear"></div>
-    <div class="grid_12 copyright">copyright 2014 - Mageia Russian Community</div>
-	<div class="clear"></div>
+<!--	<div class="footer_menu grid_2 push_6 alpha">-->
+<!--        <p>Карта сайта-->
+<!--        <ul>-->
+<!--            <li><a href="/">Главная</a></li>-->
+<!--            <li><a href="/download">Загрузки</a></li>-->
+<!--            <li><a href="/mrcrepo">Репозиторий</a></li>            -->
+<!--			<li><a href="http://forum.mageialinux.ru/">Форум</a></li>-->
+<!--			<li><a href="http://wiki.mageialinux.ru/">Вики</a></li>            -->
+<!--			<li>&nbsp;</li>-->
+<!--			<li><a href="/arch">Архив</a></li>-->
+<!--			<li><a href="/mrc-feed">mrc-feed</a></li>-->
+<!--        </ul>-->
+<!--    </div>-->
+<!--	<div class="clear"></div>-->
+<!--    <div class="grid_12 copyright">copyright 2014 - Mageia Russian Community</div>-->
+<!--	<div class="clear"></div>-->
 </div>
 <div class="clear"></div>
 </div>
