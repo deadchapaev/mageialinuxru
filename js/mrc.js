b77aea80cc2f2aff03c5cf05bffe242ef2ae5cc9
deadchapaev <chapaevcs@gmail.com>
2014-10-24 16:53:53 +0300

    Сделал более плавно листалочку

--- js/mrc.js
+++ js/mrc.js
@@ -22,23 +22,18 @@ $(document).ready(function () {
   * Листалка новостей
   */  
 $(document).on('click', '#rightarrow', function () {
-    right_carusel();
+    $first_block = $("#newsblock .news").eq(0);
+	$($first_block).hide(100, function() {
+		$($first_block).appendTo("#newsblock").show(); 
+	});
 });
 
 $(document).on('click', '#leftarrow', function () {
-    left_carusel();
-});
-
-function left_carusel() {
     $len = $("#newsblock .news").size();
 	$last_block = $("#newsblock .news").eq($len - 1);
-	$($last_block).hide()
-	$($last_block).prependTo("#newsblock").animate({ width: 'show'}, 100);
-}
+	$($last_block).hide(0, function() {
+		$($last_block).prependTo("#newsblock").show(100);
+	});
+	
+});
 
-function right_carusel() {
-	$first_block = $("#newsblock .news").eq(0);
-    $($first_block).animate({ width: 'hide' }, 100, function() {
-		$($first_block).appendTo("#newsblock").show(); 
-	}); 
-}
