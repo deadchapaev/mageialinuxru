2439b8152ff3b7b19fdfd3391429033acb1740cf
deadchapaev <chapaevcs@gmail.com>
2014-10-31 20:05:05 +0200

    Оптимизация страницы загрузок

--- download.php
+++ download.php
@@ -18,24 +18,20 @@ get_header(); ?>
             Русская и украинские локализации находятся на издании Europe 2.
         </p>
 
-        <table class="grid_10 push_1">
+        <table>
             <tr>
                 <td colspan="2" rowspan="2" class="headrow">Версия ОС</td>
                 <td rowspan="2" class="headrow">GUI</td>
                 <td rowspan="2" class="headrow">ver(bit)</td>
                 <td colspan="2" class="headrow">скачать</td>
             </tr>
-
             <tr>
                 <td class="headrow">http</td>
                 <td class="headrow">torrent</td>
             </tr>
-
-
             <tr>
                 <td colspan="6" class="osname">MAGEIA 4.1</td>
             </tr>
-
             <tr>
                 <td rowspan="2">DVD</td>
                 <td rowspan="2"  class="info">
@@ -48,23 +44,21 @@ get_header(); ?>
                     GNOME, KDE, XFCE, Mate, Cinnamon, LXDE
                 </td>
                 <td class="ver">32</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso">скачать</a>
                 </td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
-
             <tr>
                 <td class="ver">64</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso">скачать</a></td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-x86_64-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-i586-DVD.iso">скачать</a></td>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-x86_64-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
-
             <tr>
                 <td rowspan="4">liveDVD</td>
                 <td rowspan="4" class="info">
@@ -75,71 +69,63 @@ get_header(); ?>
                 </td>
                 <td rowspan="2" class="gui">KDE</td>
                 <td class="ver">32</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-i586-DVD.iso">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-i586-DVD.iso">скачать</a>
                 </td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-i586-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-i586-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
             <tr>
                 <td class="ver">64</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-x86_64-DVD.iso">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-x86_64-DVD.iso">скачать</a>
                 </td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-x86_64-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-KDE4-x86_64-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
             <tr>
                 <td rowspan="2" class="gui">GNOME</td>
                 <td class="ver">32</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-i586-DVD.iso">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-i586-DVD.iso">скачать</a>
                 </td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-i586-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-i586-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
             <tr>
                 <td class="ver">64</td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-x86_64-DVD.iso">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-x86_64-DVD.iso">скачать</a>
                 </td>
-                <td class="download"><a
-                        href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-x86_64-DVD.iso&amp;torrent=1">скачать</a>
+                <td class="download">
+					<a href="http://www.mageia.org/ru/downloads/get/?q=Mageia-4.1-LiveDVD-GNOME-x86_64-DVD.iso&amp;torrent=1">скачать</a>
                 </td>
             </tr>
         </table>
-        <div class="clear"></div>
-
-        <div id="aftertable" class="grid_12">
-
-            <p>
-                LiveDVD образ можно записать на usb-флеш-накопитель и после использовать "флешку" для загрузки системы в
-                Live-режиме или установки. Подробней можно прочитать <a
-                    href="http://forum.mageialinux.ru/viewtopic.php?id=37">здесь</a>.
-                <br><br>
-                Полный список вариантов загрузки Mageia смотри на <a href="http://www.mageia.org/ru/downloads/">официальном
-                    сайте в секции downloads</a>.
-                <br><br>
-                Если Вы выбираете образ для первого знакомства с Mageia и, возможно, первого знакомства с Linux,
-                рекомендуем
-                Вам воспользоваться LiveDVD с KDE.<br><br>
-                Если Вы не знаете версию для какой архитектуры выбрать, то отталкивайтесь от количества оперативной
-                памяти
-                вашего компьютера: менее 4 Гбайт - 32 бит, 4 и более - 64 бит.
-            </p>
-
-        </div>
-        <div class="clear"></div>
-
+        
+        <p>
+            LiveDVD образ можно записать на usb-флеш-накопитель и после использовать "флешку" для загрузки системы в
+            Live-режиме или установки. Подробней можно прочитать <a
+                href="http://forum.mageialinux.ru/viewtopic.php?id=37">здесь</a>.
+            <br><br>
+            Полный список вариантов загрузки Mageia смотри на <a href="http://www.mageia.org/ru/downloads/">официальном
+                сайте в секции downloads</a>.
+            <br><br>
+            Если Вы выбираете образ для первого знакомства с Mageia и, возможно, первого знакомства с Linux,
+            рекомендуем
+            Вам воспользоваться LiveDVD с KDE.<br><br>
+            Если Вы не знаете версию для какой архитектуры выбрать, то отталкивайтесь от количества оперативной
+            памяти
+            вашего компьютера: менее 4 Гбайт - 32 бит, 4 и более - 64 бит.
+        </p>
+        
         <div class="button"><a href="javascript:javascript:history.go(-1)">Назад</a></div>
 
-
-        <div class="clear"></div>
-
     </div>
+	<div class="clear"></div>
 
 
 <?php get_footer(); ?>
