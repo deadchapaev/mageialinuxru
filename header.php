9c5eb170a8e452b2716e2ace61ebecad9f9b1b4e
deadchapaev <chapaevcs@gmail.com>
2014-11-17 20:53:21 +0200

    Добавил rss

--- header.php
+++ header.php
@@ -9,9 +9,15 @@
 	
 	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico"/>
 
+	<link rel="alternate" type="application/rss+xml" href="<?php echo get_template_directory_uri(); ?>/rss/mageia2.xml" title="Mageia 2 MRC RSS"/>
+	<link rel="alternate" type="application/rss+xml" href="<?php echo get_template_directory_uri(); ?>/rss/mageia3.xml" title="Mageia 3 MRC RSS"/>
+	<link rel="alternate" type="application/rss+xml" href="<?php echo get_template_directory_uri(); ?>/rss/mageia4.xml" title="Mageia 4 MRC RSS"/>
+	
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.10.2.js" charset="utf-8"></script>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mrc.js" charset="utf-8"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/slide.js" charset="utf-8"></script>
+	
+	
 
 	<title>Русскоязычное сообщество пользователей Mageia</title>
 </head>
