24079d8138e2e5ca490d9e8560892a8e3af18ce9
chapaev <chapaevcs@gmail.com>
2015-09-06 10:45:26 +0300

    Подправил ссылку на мрц-фид

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
         <div id="logo"><a href="/"></a></div>
         <ul id="uppermenu">
             <li><a href="/arch">Архив</a></li>
-            <li><a href="/mrc-feed">mrc-feed</a></li>
+            <li><a href="http://repo.mageialinux.ru/">mrc-feed</a></li>
         </ul>
         
         <ul id="menu">
